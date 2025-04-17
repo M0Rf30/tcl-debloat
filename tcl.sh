2b84f4110bdcbf06575ddc372fe05d2d0e9f53b9
Gianluca Boiano <morf3089@gmail.com>
2025-04-17 17:10:53 +0200

    general enhancements

--- tcl.sh
+++ tcl.sh
@@ -1,274 +1,187 @@
-#!/usr/bin/sh
+#!/bin/bash
 set -e
+
+# -----------------------------------------------------------------------------
+# Configuration variables
+# -----------------------------------------------------------------------------
 TEMP_DIR=tmp-tcl-debloater
-FLAUNCHER_URL=https://github.com/CocoCR300/flauncher/releases/download/v2024.12.004/flauncher-armeabi-v7a-release.apk
-KODI_URL=https://mirrors.kodi.tv/releases/android/arm/kodi-21.1-Nexus-armeabi-v7a.apk
-SMARTTUBE_URL=https://github.com/yuliskov/SmartTube/releases/download/25.24s/SmartTube_stable_25.24_armeabi-v7a.apk
+FLAUNCHER_URL="https://github.com/CocoCR300/flauncher/releases/download/v2024.12.004/flauncher-armeabi-v7a-release.apk"
+KODI_URL="https://mirrors.kodi.tv/releases/android/arm/kodi-21.2-Omega-armeabi-v7a.apk"
+SMARTTUBE_URL="https://github.com/yuliskov/SmartTube/releases/download/27.03s/SmartTube_stable_27.03_armeabi-v7a.apk"
 
-BLOAT=$(
-  cat <<EOF
-android.autoinstalls.config.google.gtvpai
-au.com.stan.and
-com.amazon.amazonvideo.livingroom
-com.android.cts.ctsshim
-com.android.cts.priv.ctsshim
-com.android.dreams.basic
-com.android.hotspot2.osulogin
-com.android.htmlviewer
-com.android.providers.userdictionary
-com.android.settings.intelligence
-com.android.soundpicker
-com.android.statementservice
-com.android.vending
-com.google.android.apps.mediashell
-com.google.android.apps.nbu.smartconnect.tv
-com.google.android.apps.tv.dreamx
-com.google.android.apps.tv.launcherx
-com.google.android.feedback
-com.google.android.gms # V8-R51MT05-LF1V578 requires this for launcherx; not true for V8-R51MT05-LF1V599
-com.google.android.gsf # V8-R51MT05-LF1V578 requires this for launcherx; not true for V8-R51MT05-LF1V599
-com.google.android.inputmethod.latin
-com.google.android.katniss
-com.google.android.marvin.talkback
-com.google.android.onetimeinitializer
-com.google.android.overlay.modules.permissioncontroller
-com.google.android.partnersetup
-com.google.android.play.games
-com.google.android.sss.authbridge
-com.google.android.syncadapters.calendar
-com.google.android.tts
-com.google.android.tungsten.setupwraith
-com.google.android.videos
-com.google.android.youtube.tv
-com.google.android.youtube.tvmusic
-com.netflix.ninja
-com.tcl.accessibility
-com.tcl.android.webview
-org.chromium.webview_shell
-tv.wuaki.apptv
-EOF
+# List of packages to process
+BLOAT=(
+  android.autoinstalls.config.google.gtvpai
+  au.com.stan.and
+  com.amazon.amazonvideo.livingroom
+  com.android.cts.ctsshim
+  com.android.cts.priv.ctsshim
+  com.android.dreams.basic
+  com.android.hotspot2.osulogin
+  com.android.htmlviewer
+  com.android.providers.userdictionary
+  com.android.settings.intelligence
+  com.android.soundpicker
+  com.android.statementservice
+  com.android.vending
+  com.google.android.apps.mediashell
+  com.google.android.apps.nbu.smartconnect.tv
+  com.google.android.apps.tv.dreamx
+  com.google.android.feedback
+  com.google.android.gms
+  com.google.android.gsf
+  com.google.android.inputmethod.latin
+  com.google.android.katniss
+  com.google.android.marvin.talkback
+  com.google.android.onetimeinitializer
+  com.google.android.overlay.modules.permissioncontroller
+  com.google.android.partnersetup
+  com.google.android.play.games
+  com.google.android.sss.authbridge
+  com.google.android.syncadapters.calendar
+  com.google.android.tts
+  com.google.android.tungsten.setupwraith
+  com.google.android.tv.remote.service
+  com.google.android.videos
+  com.google.android.youtube.tv
+  com.google.android.youtube.tvmusic
+  com.netflix.ninja
+  com.tcl.accessibility
+  com.tcl.android.webview
+  org.chromium.webview_shell
+  tv.wuaki.apptv
 )
 
-BLOAT_EX=$(
-  cat <<EOF
-com.tcl.assistant
-com.tcl.autopair # seems that this disable bt rc pairing
-com.tcl.bi
-com.tcl.browser
-com.tcl.channelplus
-com.tcl.dashboard
-com.tcl.esticker
-com.tcl.gallery
-com.tcl.gamebar
-com.tcl.guard
-com.tcl.hearaid
-com.tcl.keyhelp
-com.tcl.messagebox
-com.tcl.micmanager
-com.tcl.MultiScreenInteraction_TV
-com.tcl.notereminder
-com.tcl.overseasappshow
-com.tcl.smartalexa
-com.tcl.t_solo
-com.tcl.tcl_bt_rcu_service # although the name, seems not related to rc auto-pair
-com.tcl.useragreement
-com.tcl.usercenter
-com.tcl.waterfall.overseas
-com.tcl.xian.StartandroidService
-EOF
+BLOAT_EX=(
+  com.tcl.MultiScreenInteraction_TV
+  com.tcl.assistant
+  com.tcl.bi
+  com.tcl.browser
+  com.tcl.channelplus
+  com.tcl.dashboard
+  com.tcl.esticker
+  com.tcl.gallery
+  com.tcl.gamebar
+  com.tcl.guard
+  com.tcl.hearaid
+  com.tcl.keyhelp
+  com.tcl.messagebox
+  com.tcl.micmanager
+  com.tcl.notereminder
+  com.tcl.ocean.instructions
+  com.tcl.overseasappshow
+  com.tcl.smartalexa
+  com.tcl.t_solo
+  com.tcl.tcl_bt_rcu_service # although the name, seems not related to rc auto-pair
+  com.tcl.tv.tclhome_passive
+  com.tcl.ui_mediaCenter
+  com.tcl.useragreement
+  com.tcl.usercenter
+  com.tcl.versionUpdateApp
+  com.tcl.waterfall.overseas
+  com.tcl.xian.StartandroidService
 )
 
-echo "TCL IP:"
-TCL_IP=$(cat)
-adb connect "$TCL_IP" | grep -q fail && echo "Unable to connect to TCL TV" && false
+# -----------------------------------------------------------------------------
+# Defaults & Usage
+# -----------------------------------------------------------------------------
+ACTION="list" # default action
 
-mkdir -p $TEMP_DIR
+usage() {
+  echo "Usage: $0 [--enable | --disable | --extras | --list]"
+  echo "   --enable    Enable all the packages (restores them)"
+  echo "   --disable   Disable the packages (default behavior)"
+  echo "   --extras    Enable extra packages"
+  echo "   --list      List all the packages"
+}
 
+# -----------------------------------------------------------------------------
+# Parse Arguments
+# -----------------------------------------------------------------------------
+if [ "$#" -gt 1 ]; then
+  usage
+fi
 
-echo "Downloading FLauncher"
-wget $FLAUNCHER_URL -O $TEMP_DIR/flauncher.apk
-echo "Installing FLauncher"
-adb install $TEMP_DIR/flauncher.apk
+if [ "$#" -eq 1 ]; then
+  case "$1" in
+    --enable)
+      ACTION="enable"
+      ;;
+    --disable)
+      ACTION="disable"
+      ;;
+    --extras)
+      ACTION="extras"
+      ;;
+    --list)
+      ACTION="list"
+      ;;
+    *)
+      usage
+      ;;
+  esac
+fi
 
-echo "Downloading Kodi"
-wget $KODI_URL -O $TEMP_DIR/kodi.apk
-echo "Installing Kodi"
-adb install $TEMP_DIR/kodi.apk
+# -----------------------------------------------------------------------------
+# Main script functionality
+# -----------------------------------------------------------------------------
+echo -n "TCL IP: "
+read TCL_IP
 
-echo "Downloading SmartTube Next"
-wget $SMARTTUBE_URL -O $TEMP_DIR/stn.apk
-echo "Installing SmartTube Next"
-adb install $TEMP_DIR/stn.apk
+if ! adb connect "$TCL_IP" | grep -q "connected"; then
+  echo "Unable to connect to TCL TV"
+  exit 1
+fi
 
-rm -r $TEMP_DIR
+# Download and install Kodi and SmartTube Next extras.
+if [ "$ACTION" = "extras" ]; then
+  mkdir -p "$TEMP_DIR"
 
-for package in $BLOAT; do
-  echo "Disabling ${package}"
-  adb shell pm disable-user --user 0 "$package" | true
-done
+  echo "Downloading Kodi"
+  wget "$KODI_URL" -O "$TEMP_DIR/kodi.apk"
+  echo "Installing Kodi"
+  adb install "$TEMP_DIR/kodi.apk"
 
-for package in $BLOAT_EX; do
-  echo "Disabling ${package}"
-  adb shell pm disable-user --user 0 "$package" | true
-done
+  echo "Downloading SmartTube Next"
+  wget "$SMARTTUBE_URL" -O "$TEMP_DIR/stn.apk"
+  echo "Installing SmartTube Next"
+  adb install "$TEMP_DIR/stn.apk"
 
-# restore uninstalled packages
-# for package in $BLOAT
-# do
-# 	echo "enable ${package}"
-# 	adb shell pm enable $package | true
-# done
+  rm -r "$TEMP_DIR"
+fi
 
-# restore uninstalled packages
-# for package in $BLOAT_EX
-# do
-# 	echo "enable ${package}"
-# 	adb shell pm enable $package | true
-# done
+if [ "$ACTION" = "list" ]; then
+  echo "Listing all packages"
+  adb shell pm list packages
+fi
+
+# Loop through packages and either disable or enable them
+process_packages() {
+  local action_command="$1"
+  shift
+  local package_list=("$@")
+  for package in "${package_list[@]}"; do
+    echo "${action_command^}ing ${package}"
+    # suppress failure error output with "| true" in case the package is already in the desired state
+    adb shell pm "$action_command" "$package" || true
+  done
+}
+
+if [ "$ACTION" = "disable" ]; then
+  process_packages disable-user "${BLOAT[@]}"
+  process_packages disable-user "${BLOAT_EX[@]}"
+elif [ "$ACTION" = "enable" ]; then
+  process_packages enable "${BLOAT[@]}"
+  process_packages enable "${BLOAT_EX[@]}"
+fi
+
+echo "Done."
 
 # Not sure or needed
 # com.android.providers.downloads
 # com.android.providers.media
 # com.android.providers.media.module
+# com.tcl.autopair # seems that this disable bt rc pairing
 # com.tcl.inputmethod.international
 # com.tcl.miracast # disabling this, no screen sharing
 # com.tcl.partnercustomizer # disabling this, kill rc tv button
-# com.tcl.tv.tclhome_passive
-# com.tcl.versionUpdateApp
-
-
-# Entire list on 50C6459
-# adb shell pm list packages | sort
-
-# android
-# android.autoinstalls.config.google.gtvpai
-# au.com.stan.and
-# com.amazon.amazonvideo.livingroom
-# com.android.backupconfirm
-# com.android.bluetooth
-# com.android.camera2
-# com.android.captiveportallogin
-# com.android.certinstaller
-# com.android.companiondevicemanager
-# com.android.cts.ctsshim
-# com.android.cts.priv.ctsshim
-# com.android.dreams.basic
-# com.android.dynsystem
-# com.android.externalstorage
-# com.android.hotspot2.osulogin
-# com.android.htmlviewer
-# com.android.inputdevices
-# com.android.keychain
-# com.android.localtransport
-# com.android.location.fused
-# com.android.messaging
-# com.android.networkstack.inprocess
-# com.android.networkstack.permissionconfig
-# com.android.networkstack.tethering.inprocess
-# com.android.pacprocessor
-# com.android.printspooler
-# com.android.providers.calendar
-# com.android.providers.contacts
-# com.android.providers.downloads
-# com.android.providers.media
-# com.android.providers.media.module
-# com.android.providers.settings
-# com.android.providers.tv
-# com.android.providers.userdictionary
-# com.android.proxyhandler
-# com.android.se
-# com.android.settings.intelligence
-# com.android.sharedstoragebackup
-# com.android.shell
-# com.android.soundpicker
-# com.android.statementservice
-# com.android.systemui
-# com.android.tethering.overlay
-# com.android.tethering.overlay.gsi
-# com.android.tv.settings
-# com.android.tv.settings.google.resoverlay
-# com.android.tv.settings.vendor.resoverlay
-# com.android.vending
-# com.android.vpndialogs
-# com.android.wallpaperbackup
-# com.android.wifi.resources
-# com.google.android.apps.mediashell
-# com.google.android.apps.nbu.smartconnect.tv
-# com.google.android.apps.tv.dreamx
-# com.google.android.apps.tv.launcherx
-# com.google.android.ext.services
-# com.google.android.ext.shared
-# com.google.android.feedback
-# com.google.android.gms
-# com.google.android.gsf
-# com.google.android.inputmethod.latin
-# com.google.android.katniss
-# com.google.android.marvin.talkback
-# com.google.android.modulemetadata
-# com.google.android.onetimeinitializer
-# com.google.android.overlay.modules.ext.services
-# com.google.android.overlay.modules.modulemetadata.forframework
-# com.google.android.overlay.modules.permissioncontroller
-# com.google.android.overlay.modules.permissioncontroller.forframework
-# com.google.android.packageinstaller
-# com.google.android.partnersetup
-# com.google.android.permissioncontroller
-# com.google.android.play.games
-# com.google.android.sss.authbridge
-# com.google.android.syncadapters.calendar
-# com.google.android.tts
-# com.google.android.tungsten.setupwraith
-# com.google.android.tv.assistant
-# com.google.android.tv.frameworkpackagestubs
-# com.google.android.tv.remote.service
-# com.google.android.videos
-# com.google.android.webview
-# com.google.android.youtube.tv
-# com.google.android.youtube.tvmusic
-# com.netflix.ninja
-# com.tcl.accessibility
-# com.tcl.android.webview
-# com.tcl.assistant
-# com.tcl.autopair
-# com.tcl.bi
-# com.tcl.browser
-# com.tcl.cast.framework
-# com.tcl.channelplus
-# com.tcl.common.shortcutmenu
-# com.tcl.dashboard
-# com.tcl.esticker
-# com.tcl.factory.view
-# com.tcl.gallery
-# com.tcl.gamebar
-# com.tcl.guard
-# com.tcl.hearaid
-# com.tcl.initsetup
-# com.tcl.inputmethod.international
-# com.tcl.keyhelp
-# com.tcl.logkit
-# com.tcl.messagebox
-# com.tcl.micmanager
-# com.tcl.miracast
-# com.tcl.MultiScreenInteraction_TV
-# com.tcl.notereminder
-# com.tcl.overseasappshow
-# com.tcl.partnercustomizer
-# com.tcl.providers.config
-# com.tcl.settings
-# com.tcl.smartalexa
-# com.tcl.suspension
-# com.tcl.system.server
-# com.tcl.t_solo
-# com.tcl.tcl_bt_rcu_service
-# com.tcl.tv
-# com.tcl.tv.tclhome_passive
-# com.tcl.tvinput
-# com.tcl.ui_mediaCenter
-# com.tcl.useragreement
-# com.tcl.usercenter
-# com.tcl.versionUpdateApp
-# com.tcl.waterfall.overseas
-# com.tcl.xian.StartandroidService
-# com.tvos
-# org.chromium.webview_shell
-# tv.wuaki.apptv
